938f9aad98aac27d961cfe523b615a40bad18e31
Denis Perov <denis.perov@gmail.com>
2017-02-20 00:31:29 +0300

    update modal template

--- components/modal.js
+++ components/modal.js
@@ -5,5 +5,9 @@ import './modal.html';
 Template.modal.onRendered(function() {
 	// Initially hidden if not set otherwise
 	const options = _.extend({ show: false }, this.data.options);
-	this.$(`.modal`).modal(options);
+	this.$('.modal').modal(options);
+});
+
+Template.modal.onDestroyed(function() {
+  this.$('.modal').modal('destroy');
 });
--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'majus:bootstrap-helpers',
-  version: '0.0.1',
+  name: 'imajus:bootstrap-helpers',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Bootstrap Blaze helpers and snippets',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/imajus/meteor-bootstrap-helpers.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null//'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.2');
-  api.use('ecmascript');
-  api.use(['templating'], 'client');
+  api.use('ecmascript@0.6.3');
+  api.use(['templating@1.3.0'], 'client');
   api.mainModule('bootstrap-helpers.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('majus:bootstrap-helpers', 'client');
+  api.use('ecmascript@0.6.3');
+  api.use('tinytest@1.0.12');
+  api.use('imajus:bootstrap-helpers', 'client');
   api.mainModule('bootstrap-helpers-tests.js', 'client');
 });
