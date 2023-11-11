de90970cd8e5cc9066b14c94cc58bb235956c070
Denis Perov <denis.perov@gmail.com>
2023-11-11 23:56:03 +0300

    Support more properties for {{i}} component

--- components/icon.js
+++ components/icon.js
@@ -1,1 +1,22 @@
+import { Template } from 'meteor/templating';
 import './icon.html';
+
+Template.i.helpers({
+  faAtts() {
+    const { fa, fixed, size, spin, pulse, border, pull, rotate, flip, extra } = Template.currentData();
+    return {
+      class: [
+        `fa fa-${fa}`,
+        fixed && 'fw',
+        size && `fa-${size}`,
+        spin && 'fa-spin',
+        pulse && 'fa-pulse',
+        border && 'fa-border',
+        pull && `fa-pull-${pull}`,
+        rotate && `fa-rotate-${rotate}`,
+        flip && `fa-flip-${flip}`,
+        extra,
+      ].filter(Boolean).join(' '),
+    };
+  },
+});
