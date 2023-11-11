9968496ffd9dcc2dd15cc0ac5161488f8d750d95
Denis Perov <denis.perov@gmail.com>
2023-11-12 01:00:02 +0300

    Bump up package version to 0.0.9

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'imajus:bootstrap-helpers',
-  version: '0.0.8',
+  version: '0.0.9',
   summary: 'Bootstrap 3/4 Blaze snippets',
   git: 'https://github.com/imajus/meteor-bootstrap-helpers.git',
   documentation: 'README.md'
@@ -10,11 +10,11 @@ Package.onUse(function(api) {
   api.versionsFrom('2.8.0');
   api.use('ecmascript');
   api.use([
-    'templating',
-    'imajus:helpers-core',
-    'imajus:common-helpers',
+    'templating@1.4.2',
+    'imajus:helpers-core@0.0.2',
+    'imajus:common-helpers@0.0.3',
     'tracker',
-    'blaze'
+    'blaze@2.7.1'
   ], 'client');
   api.addFiles([
     'example/bootstrap4.js',
